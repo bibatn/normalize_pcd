be6b5a6de60f3361e94001c9f0489f26329497cb
bibatn <habibulinmarat@icloud.com>
2023-02-15 13:55:56 +0300

    Done.

--- normalize.py
+++ normalize.py
@@ -1,7 +1,6 @@
 import open3d as o3d
 import numpy as np
-
-
+import sys
 def normalize_pc(points):
     centroid = np.mean(points, axis=0)
     points -= centroid
@@ -9,15 +8,25 @@ def normalize_pc(points):
     points /= furthest_distance
     return points
 
-def process():
-    pcd = o3d.io.read_point_cloud('/home/m/projects_/normalize/samples_for_test/bunny/bunny70k.pcd')
+def process(in_file, out_file=''):
+    if out_file=='':
+        out_file = in_file[:-4] + '_normalized.pcd'
+    pcd = o3d.io.read_point_cloud(in_file)
     pcd.remove_non_finite_points()
     xyz = pcd.points
     xyz = np.array(xyz)
     normalize_pc(xyz)
     normalized_pcd = o3d.geometry.PointCloud()
     normalized_pcd.points = o3d.utility.Vector3dVector(xyz)
-    o3d.io.write_point_cloud('/home/m/projects_/normalize/samples_for_test/bunny/bunny70k_normalized.pcd', normalized_pcd, write_ascii=True)
+    o3d.io.write_point_cloud(out_file, normalized_pcd, write_ascii=True)
+
+def print_usage():
+    print('python3 normalize.py [.pcd filename]')
 
 if __name__ == '__main__':
-    process()
+    if(len(sys.argv)==1):
+        print_usage()
+    elif(len(sys.argv)==2):
+        process(sys.argv[1])
+    elif(len(sys.argv)==3):
+        process(sys.argv[1], sys.argv[2])
