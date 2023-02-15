3dd42f934012d6ffd17929ff5df095db4e3fde53
bibatn <habibulinmarat@icloud.com>
2023-02-15 11:48:56 +0300

    Working_prototype.

--- normalize.py
+++ normalize.py
@@ -14,4 +14,10 @@ def process():
     pcd.remove_non_finite_points()
     xyz = pcd.points
     xyz = np.array(xyz)
+    normalize_pc(xyz)
+    normalized_pcd = o3d.geometry.PointCloud()
+    normalized_pcd.points = o3d.utility.Vector3dVector(xyz)
+    o3d.io.write_point_cloud('/home/m/projects_/normalize/samples_for_test/bunny/bunny70k_normalized.pcd', normalized_pcd, write_ascii=True)
 
+if __name__ == '__main__':
+    process()
