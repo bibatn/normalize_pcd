6701c8348f9974803cee7e96c8afd94a0bbc0b51
bibatn <habibulinmarat@icloud.com>
2023-02-17 18:58:35 +0300

    Правки.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ http://www.open3d.org/docs/0.9.0/tutorial/Basic/file_io.html
 python3 -m venv env  
 . env/bin/activate  
 pip install open3d  
-python3 normalize.py
+python3 normalize.py [input.pcd] [output.pcd]
--- normalize.py
+++ normalize.py
@@ -1,12 +1,15 @@
 import open3d as o3d
 import numpy as np
 import sys
+
+
 def normalize_pc(points):
     centroid = np.mean(points, axis=0)
     points -= centroid
     furthest_distance = np.max(np.sqrt(np.sum(abs(points)**2,axis=-1)))
     points /= furthest_distance
     return points
+
 
 def process(in_file, out_file=''):
     if out_file=='':
@@ -20,8 +23,10 @@ def process(in_file, out_file=''):
     normalized_pcd.points = o3d.utility.Vector3dVector(xyz)
     o3d.io.write_point_cloud(out_file, normalized_pcd, write_ascii=True)
 
+
 def print_usage():
     print('python3 normalize.py [.pcd filename]')
+
 
 if __name__ == '__main__':
     if(len(sys.argv)==1):
